aa24755ad1a3b44a054b02f05e8c05c9179f8b58
karansharmaa <89495471+karansharmaa@users.noreply.github.com>
2024-11-29 20:47:27 -0800

    Reshuffling of files

--- app.py
+++ app.py
@@ -1,57 +1,7 @@
-'''from flask import Flask, request, jsonify
-#from your_nlp_module import get_review_summary, get_review_rating  # assuming you have a function to handle processing
-from flask_cors import CORS
-CORS(app)
-
-app = Flask(__name__)
-
-@app.route('/api/review', methods=['POST'])
-def review():
-    data = request.get_json()
-    media_type = data.get('mediaType')
-    media_name = data.get('mediaName')
-
-    # NLP processing to get rating and summary
-    rating = get_review_rating(media_type, media_name)
-    summary = get_review_summary(media_type, media_name)
-
-    return jsonify({"rating": rating, "summary": summary})
-
-if __name__ == '__main__':
-    app.run(debug=True) '''
-
-'''from flask import Flask, render_template, request, jsonify
-
-app = Flask(__name__)
-
-# Route for serving the HTML
-@app.route('/')
-def index():
-    return render_template('index.html')
-
-# Route for processing the review request
-@app.route('/generate-review', methods=['POST'])
-def generate_review():
-    data = request.json
-    media_type = data.get('mediaType')
-    media_name = data.get('mediaName')
-
-    # Replace this logic with your NLP or scraping code
-    aggregated_rating = ""
-    summary_review = ""
-
-    return jsonify({
-        "rating": aggregated_rating,
-        "summary": summary_review
-    })
-
-if __name__ == '__main__':
-    app.run(debug=True)'''
-
 from flask import Flask, render_template, request, jsonify
 import os
 import pandas as pd
-from choose_movie import create_movie_list, get_movie_sentiment_analysis
+from src.choose_movie import create_movie_list, get_movie_sentiment_analysis
 
 app = Flask(__name__)
 
